e9e72a27d5940ae15d39a99ad565bb15b2bcc817
Samuel <unclebay143@gmail.com>
2020-09-20 19:07:25 +0100

    At this point we can add a client and the client can add an employee -- PACE is ready for presentation

--- assets/js/registration.js
+++ assets/js/registration.js
@@ -1,14 +1,14 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("form2_container");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var revoke_btn = document.getElementById("form_2Btn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close_form2")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+revoke_btn.onclick = function() {
   modal.style.display = "block";
 }
 
