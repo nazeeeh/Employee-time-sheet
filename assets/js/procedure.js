4f474071eaf03666039b693b7a05c236fe1e395b
Samuel <unclebay143@gmail.com>
2020-09-27 14:29:01 +0100

    able to store information to local storage

--- assets/js/procedure.js
+++ assets/js/procedure.js
@@ -1,10 +1,26 @@
 var email = document.getElementById("email");
 var password = document.getElementById("password");
-var url_link = document.getElementById("url_link");
-document.getElementById("demo_btn").addEventListener("click", function() {
-  if (email.value != null){
+var url = document.getElementById("url");
+var name = document.getElementById("name");
+var tel = document.getElementById("phone");
+var type = document.getElementById("type");
+var logo = document.getElementById("logo");
+
+let validateReg = () => 
+{
+
+  if (email.value == "" || password.value == "" || url.value == "" || name.value == "" || tel.value == "" || type.value == ""){
+    
+    alert(`All Field are required
+    Email: ${email.value}
+    Company URL: ${url.value}`)
+  }
+  else{
+    // alert(url.value)
     const check = confirm(
-      `Continue to register as ${email.value} with company url ${url_link.value}?`);
+      `Continue to register as 
+      Email: ${email.value}
+      Company url: ${url.value}?`);
     if (check == false)
     {
       // to close the alert only when user click cancel
@@ -17,8 +33,6 @@ document.getElementById("demo_btn").addEventListener("click", function() {
       alert(`Redirecting ${email.value} to Dashboard: `)
     }
   }
-  else{
-    alert(`Email ${email.value} is not valid`)
-  }
-});
+
+};
 
---
+++ assets/js/userAuth.js
@@ -0,0 +1,25 @@
+// let emailAuth = document.getElementById("emailAuth").value;
+// var pwAuth = document.getElementById("passwordAuth").value;
+
+// let paceDB = JSON.parse(localStorage.getItem("paceDB"));
+// isExist = paceDB.find(findEmail)
+// if(isExist )
+// {
+
+//   alert("i dey here")
+
+// }
+// else
+// {
+
+//   alert("not found")
+
+// }
+
+// function findEmail(x)
+// {
+
+//   x.email == "test1@gmail.com";
+
+// }
+
