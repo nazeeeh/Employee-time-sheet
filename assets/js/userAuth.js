7b3dd4b8918f1be884a08426fd0e1647f4b262ca
Samuel <unclebay143@gmail.com>
2020-09-29 19:28:44 +0100

    Figured more proper way of rendering current user data from the local storage

--- assets/js/userAuth.js
+++ assets/js/userAuth.js
@@ -20,16 +20,33 @@ let authUser = () =>
   email_InDb = paceDB.find(x=> x.email == emailAuth);
   password_InDb = paceDB.find(x=> x.password == pwAuth);
   
-  console.log(email_InDb["email"])
-  console.log(password_InDb["password"])
+  // console.log(email_InDb["email"])
+  // console.log(password_InDb["password"])
 
   if(emailAuth == email_InDb["email"] && pwAuth == password_InDb["password"]){
 
     // window.location.href = "https://pacetimesheet.netlify.app/contents/confirmation.html", 500;
 
+    // current loggedin user
+
+    let current_UserDb= JSON.parse(localStorage.getItem("currentUser"));
+    if(current_UserDb == null || current_UserDb.length > 1) // if paceDB does not exist create one 
+    {
+
+      current_UserDb=[]
+
+    }
+    current_UserDb.push(email_InDb)
+    localStorage.setItem("currentUser", JSON.stringify(current_UserDb))
+    
     location.assign("../contents/internal-dashboard.html"); //redirect to internal dashboard
     
   }
+  // else if(emailAuth === undefined || pwAuth === undefined){
+
+  //   unKnown.innerHTML = "Invalid Login Details"
+  
+  // }
   else
   {
 
--- assets/js/userDetails.js
+++ assets/js/userDetails.js
@@ -1,16 +1,16 @@
-let paceDB = JSON.parse(localStorage.getItem("paceDB"));
+let islogged_In = JSON.parse(localStorage.getItem("currentUser"));
 
-document.getElementById("name").innerHTML = paceDB[0].name;
+document.getElementById("name").innerHTML = islogged_In[0].name;
 // alert(paceDB[0].logo)
 
 
-  document.getElementById("logo").innerHTML = `<img id="companyImage" height = "100px" src="../assets/img/register/${paceDB[0].logo}">`
+  document.getElementById("logo").innerHTML = `<img id="companyImage" height = "100px" src="../assets/img/register/${islogged_In[0].logo}">`
 
 
 
 let logOut = () =>
 {
 
-  localStorage.clear("paceDB");
+  localStorage.clear("currentUser");
 
 }
