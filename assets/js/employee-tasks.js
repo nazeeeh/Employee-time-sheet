8f9f5944c5854ec8eb1cdb74226a871f8346b0a4
Aeesh <adamaisha352@gmail.com>
2020-10-03 18:26:19 +0100

    modified table

--- assets/js/employee-tasks.js
+++ assets/js/employee-tasks.js
@@ -1,3 +1,16 @@
 function closeTasks() {
     location.assign("../contents/employee-dashboard.html");
 }
+
+
+function see () {
+    eTasks = JSON.parse(localStorage.getItem("tasks"))
+
+    if(eTasks==null) {
+        eTasks = [];
+    }
+    eData = JSON.stringify(eTasks)
+alert(eData);
+}
+
+// i need to get the data from the local storage then check if it matches the employee's name and save it
