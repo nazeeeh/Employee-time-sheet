f0874af3d6deb756746f379968fdb14036fd9944
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2020-10-08 19:27:38 +0100

    You can add a task, employee recieves task, employee approves task, task gets deleted from employee 'see all tasks' screen, deleted from listed of pending and status changes to 'accepted'

--- assets/js/maintask.js
+++ assets/js/maintask.js
@@ -22,7 +22,7 @@ function displayMain(task){
     for(i = 0; i < task.length; i++){
         add += `<div class="main-task" id="${i}">
         <p> <strong> Task name : </strong> ${task[i].name} <span>Assigned to : ${task[i].employee}</span> </p> 
-        <div id="dateAndButtons"><div id="date"><span class="date">Due : ${task[i].due}</span> <span>Status : Pending</span></div> <div id="buttons"><button onclick="deletesAssigned(${i})">Delete</button> <button onclick="showEditButton(${i})">Edit</button></div> </div>
+        <div id="dateAndButtons"><div id="date"><span class="date">Due : ${task[i].due}</span> <span>Status : ${task[i].status}</span></div> <div id="buttons"><button onclick="deletesAssigned(${i})">Delete</button> <button onclick="showEditButton(${i})">Edit</button></div> </div>
         <div id="editMainTask${i}" class="edit-Main">
         <input type="text" id="edit-name-main${i}" placeholder="Edit Task"  required>
         <input type="text" id="edit-employee-main${i}" placeholder="Edit employee assigned" required>
@@ -60,7 +60,8 @@ function appendNewTaskMain(){
         newTask = {
             name : taskName,
             employee : employeeAssigned,
-            due : dueDate
+            due : dueDate,
+            status : "Pending"
         }
 
         tasks.push(newTask)
