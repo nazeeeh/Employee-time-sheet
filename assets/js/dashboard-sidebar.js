5e9f158a1e16b6748abb961a260b655972ee815f
Peter Adeyemo <adeyemodanointed5@gmail.com>
2020-10-15 09:54:30 +0100

    All broken link sorted

--- assets/js/dashboard-sidebar.js
+++ assets/js/dashboard-sidebar.js
@@ -15,5 +15,10 @@ function myFunction() {
   }
 }
 
-
+// clear the temporary local storage 
+let logOut = () =>
+{
+  localStorage.removeItem("currentUser");
+  location.assign('../contents/logout-confirmation.html')
+}
 // alert("working")
--- assets/js/userAuth.js
+++ assets/js/userAuth.js
@@ -194,9 +194,3 @@ let authUser = () =>
 // }
 
 
-// clear the temporary local storage 
-let logOut = () =>
-{
-  localStorage.removeItem("currentUser");
-  location.assign('../contents/logout-confirmation.html')
-}
