ef37d2f21129e534ce9ccdf1fddab82ee8316e30
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2020-10-18 16:59:22 +0100

    each user now has a her/his own task and employee search works

--- assets/js/maintask.js
+++ assets/js/maintask.js
@@ -16,10 +16,11 @@ let unassignedMainList = JSON.parse(localStorage.getItem("unassignedMainList"));
 let assignedMainList = JSON.parse(localStorage.getItem("assignedMainList"));
 let pending = JSON.parse(localStorage.getItem("pending"));
 let companyName =  _is_Login_Admin[0].name;
-let employees = JSON.parse(localStorage.getItem(`${companyName}_employees`))
+let employees = JSON.parse(localStorage.getItem(`${companyName}_employees`));
+let currentUser = JSON.parse(localStorage.getItem("current_InternalUser"))
+let currentUserEmail = currentUser[0].email
+let employeeTask = JSON.parse(localStorage.getItem(`${currentUserEmail}_task`))
 // employeeDB = JSON.parse(employees)
-
-alert(JSON.stringify(employees))
 
 if(JSON.parse(localStorage.getItem("tasks")) == null){
     tasks = []
@@ -41,8 +42,13 @@ if(employees == null){
     employees = []
 }
 
+if(JSON.parse(localStorage.getItem(`${currentUserEmail.email}_task`)) == null || undefined){
+    employeeTask = []
+}
+
+// alert(currentUserEmail)
 displayUnassigned(unassignedMainList)
-displayMain(assignedMainList)
+displayMain(employeeTask)
        
 function displayMain(task){
     let add = ''
@@ -54,7 +60,7 @@ function displayMain(task){
         <input type="text" id="edit-name-main${i}" placeholder="Edit Task"  required>
         <input type="text" id="edit-employee-main${i}" placeholder="Edit employee assigned" required>
         <input type="date" id="edit-due-date-main${i}" placeholder="Edit due date" required> <br>
-        <button id="assign" onclick="editAssigned(${i})">Edit</button> <button id="cancel" onclick="cancelEdit(${i})">Cancel</button>
+        <button id="assign" onclick="editAssigned(${i})">Edit</button> <button id="cancel" onclick="CloseEditButton(${i})">Cancel</button>
         </div>
       </div>
        `
@@ -90,10 +96,14 @@ function appendNewTaskMain(){
             due : dueDate,
             status : "Pending"
         }
-
         tasks.push(newTask)
+        employeeTask.push(newTask)
         localStorage.setItem("tasks",  JSON.stringify(tasks))
-    
+        localStorage.setItem(`${currentUserEmail}_task`, JSON.stringify(employeeTask))
+        employeeNames = []
+        for(i = 0; i < employees.length; i++){
+            employeeNames.push(employees[i].name)
+        }
         if(employeeAssigned == ""){
             unassignedMainList.push(newTask)
             localStorage.setItem("unassignedMainList",  JSON.stringify(unassignedMainList))
@@ -101,10 +111,15 @@ function appendNewTaskMain(){
         }else if(employeeAssigned != ""){
             assignedMainList.push(newTask)
             pending.push(newTask)
+            // if (employeeAssigned in employeeNames){
+                
+            // }
             localStorage.setItem("assignedMainList",  JSON.stringify(assignedMainList))
             localStorage.setItem("pending",  JSON.stringify(pending))
-            displayMain(assignedMainList)
+            displayMain(employeeTask)
         }
+        alert(JSON.stringify(employeeTask))
+
     }
     CancelTask()
    
@@ -120,17 +135,22 @@ function CancelTask(){
 }
 
 function deletesAssigned(id){
-    userIndex = tasks.findIndex(x => x.name == assignedMainList[id].name)
-    tasks.splice(userIndex, 1)
-    localStorage.setItem("tasks",  JSON.stringify(tasks))
+    // userIndex = tasks.findIndex(x => x.name == assignedMainList[id].name)
+    // tasks.splice(userIndex, 1)
+    // localStorage.setItem("tasks",  JSON.stringify(tasks))
 
-    userIndex = pending.findIndex(x => x.name == assignedMainList[id].name)
-    pending.splice(userIndex, 1)
-    localStorage.setItem("pending",  JSON.stringify(pending))
+    // userIndex = pending.findIndex(x => x.name == assignedMainList[id].name)
+    // pending.splice(userIndex, 1)
+    // localStorage.setItem("pending",  JSON.stringify(pending))
 
-    assignedMainList.splice(id, 1)
-    localStorage.setItem("assignedMainList",  JSON.stringify(assignedMainList))
-    displayMain(assignedMainList)
+    employeeTask.splice(id, 1)
+    // localStorage.setItem("assignedMainList",  JSON.stringify(assignedMainList))
+
+    // userIndex2 = employeeTask.findIndex(x => x.name == assignedMainList[id].name)
+    // employeeTask.splice(userIndex2, 1);
+    localStorage.setItem(`${currentUserEmail}_task`, JSON.stringify(employeeTask))
+    // alert(userIndex2)
+    displayMain(employeeTask)
 }
 
 function deletesUnassigned(id){
@@ -141,6 +161,12 @@ function deletesUnassigned(id){
     unassignedMainList.splice(id, 1)
     localStorage.setItem("unassignedMainList",  JSON.stringify(unassignedMainList))
     displayUnassigned(unassignedMainList)
+
+    userIndex2 = employeeTask.findIndex(x => x.name == tasks[id].name)
+    localStorage.setItem(`${currentUserEmail}_task`, JSON.stringify(employeeTask))
+    employeeTask.splice(userIndex2, 1);
+    alert(userIndex2)
+
 }
 
 function showEditButton(id){
@@ -151,78 +177,106 @@ function showEditButton(id){
     }else{editForm.style.display = "block"}
 }
 
+function CloseEditButton(id){
+    let editForm = document.getElementById("editMainTask" + id)
+
+    if (editForm.style.display == "none"){
+        editForm.style.display = "block"
+    }else{editForm.style.display = "none"}
+}
+
 function editAssigned(id){
     let editName = document.getElementById("edit-name-main" + id).value;
     let editEmployee = document.getElementById("edit-employee-main" + id).value;
     let editDueDate = document.getElementById("edit-due-date-main" + id).value;
+    // console.log(employeeTask[userIndex2], tasks)
 
     userIndex = tasks.findIndex(x => x.name == assignedMainList[id].name)
-
+    // userIndex2 = employeeTask.findIndex(x => x.name == tasks[id].name)
     if(editName === ""){
-        tasks[userIndex].name = tasks[userIndex].name
-        assignedMainList[id].taskName = assignedMainList[id].name
-    } else{tasks[userIndex].name = editName
-        assignedMainList[id].name = editName
+        // tasks[userIndex].name = tasks[userIndex].name
+        employeeTask[id].name = employeeTask[id].name
+        // employeeTask[userIndex2].name = employeeTask[userIndex2].name
+    } else{employeeTask[id].name = editName
+        // employeeTask[id].name = editName
+        // employeeTask[userIndex2].name = editName
     }
 
     if( editEmployee === ""){
-        tasks[userIndex].employee = tasks[userIndex].employee
-        assignedMainList[id].employee = assignedMainList[id].employee
-    } else{tasks[userIndex].employee = editEmployee
-        assignedMainList[id].employee = editEmployee
+        // tasks[userIndex].employee = tasks[userIndex].employee
+        employeeTask[id].employee = employeeTask[id].employee
+        // employeeTask[userIndex2].employee = employeeTask[userIndex2].employee
+    } else{
+        // tasks[userIndex].employee = editEmployee
+        employeeTask[id].employee = editEmployee
+        // employeeTask[userIndex2].employee = editEmployee
     }
 
     if(editDueDate === ""){
-        tasks[userIndex].due = tasks[userIndex].due
-        assignedMainList[id].due = assignedMainList[id].due
-    }else{tasks[userIndex].due = editDueDate
-        assignedMainList[id].due = editDueDate
+        // tasks[userIndex].due = tasks[userIndex].due
+        employeeTask[id].due = employeeTask[id].due
+        // employeeTask[userIndex2].due = employeeTask[userIndex2].due
+    }else{
+        // tasks[userIndex].due = editDueDate
+        employeeTask[id].due = editDueDate
+        // employeeTask[userIndex2].due = editDueDate
     }
 
     localStorage.setItem("tasks",  JSON.stringify(tasks))
     localStorage.setItem("unassignedMainList",  JSON.stringify(unassignedMainList))
-    displayMain(assignedMainList)
+    localStorage.setItem(`${currentUserEmail}_task`, JSON.stringify(employeeTask))
+    displayMain(employeeTask)
 }
-var foundele = []
-var found2 = []
 
-function showEmployee(){
-    employeeAssigned = document.getElementById("employee-assigned-main")
-    found = []
-    employeeAssigned.addEventListener('keydown', displayOptions)
-    let searchDetails = employeeAssigned.value
+    document.getElementById("employee-assigned-main").addEventListener('keyup', displayOptions)
+    
     function displayOptions(){
-        found = (employees.filter(x => (x.name).toLowerCase().includes((searchDetails))))
-        alert(JSON.stringify(found))
-        for(i = 0; i < found.length; i++){
-            foundele.push(found[i].name)
+    let employeeName = document.getElementById("employee-assigned-main").value
+    let searchResult = document.querySelector('.employee')
+    employeeNames = []
+        for(i = 0; i < employees.length; i++){
+            employeeNames.push(employees[i].name)
         }
-        found2 = foundele.filter(x => x.toLowerCase().includes((searchDetails)))
-        console.log(found2)
-        displayEmployee()
+        searchResult.innerHTML = ''
+        let employeeResult = employeeNames.filter(function(employee){
+            return employee.toLowerCase().startsWith(employeeName.toLowerCase());
+        });
 
+        employeeResult.forEach(element => {
+            i = employeeResult.indexOf(element)
+            let div =document.createElement('div')
+            div.innerHTML = element
+            searchResult.appendChild(div)
+            div.setAttribute("id", `${i}`)
+            div.setAttribute('onclick', `appendName(${i})`)
+        });
+
+        if (employeeName == ""){
+            searchResult.innerHTML = ''
+        }
+
+        return employeeResult
     }
   
-}
 
 function appendName(id){
-    document.getElementById("employee").style.display == "none"
-    employeeAssigned = document.getElementById("employee-assigned-main")
-    employeeAssigned.value = found[id].name
-
+    results = displayOptions()
+    // console.log(results)
+    let searchResult = document.querySelector('.employee')
+    // console.log(results, id)
+    document.getElementById("employee-assigned-main").value = results[id]
+    searchResult.innerHTML = ''
 }
 
-function displayEmployee(){
-    // document.getElementById("employee").style.display == "block"
-    let add = ''
-    for(i = 0; i < found.length; i++){
-        add += `<div id="${i}" class="resultsEmployee" onclick="appendName(${i})">
-        <p>${found[i].name}</p>
-        </div>
-        <hr>
-        `
-    }
-    document.getElementById("employee").innerHTML = add
-}
-
-showEmployee()
+// function displayEmployee(){
+//     // document.getElementById("employee").style.display == "block"
+//     let add = ''
+//     for(i = 0; i < found.length; i++){
+//         add += `<div id="${i}" class="resultsEmployee" onclick="appendName(${i})">
+//         <p>${found[i].name}</p>
+//         </div>
+//         <hr>
+//         `
+//     }
+//     document.getElementById("employee").innerHTML = add
+// }
